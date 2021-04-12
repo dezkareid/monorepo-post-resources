8976be3bdcce12ad816826e55497fdfe14d4c358
Joel Humberto Gomez Paredes <elmaildeldezkareid@gmail.com>
2021-04-12 03:45:32 -0500

    fix(feature-flags): fix releasing with npm

--- packages/feature-flags/release.config.js
+++ packages/feature-flags/release.config.js
@@ -7,6 +7,12 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
+    [
       '@semantic-release/git',
       {
         message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
